88c99a6eabe37dd77fc0d7935555a399074725c9
Tycho (Ty) Eggen <tycho.eggen@gmail.com>
2014-09-19 14:04:48 -0700

    New chaser script

---
+++ backend/domeplayer/scripts/chaser-white-fast.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+import time
+import random
+import base64
+
+""" Fast chaser pattern test """
+
+HOST, PORT = "localhost", 9999
+FIXTURES = 260
+
+print "FIXTURES:",FIXTURES
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+while True:
+	for light in range( 0, FIXTURES ):
+		data = ''
+		for j in range(0,light):
+			data = data + chr(0) ## Red
+			data = data + chr(0) ## Green
+			data = data + chr(0) ## Blue
+	
+		data = data + chr(255) ## Red
+		data = data + chr(255) ## Green
+		data = data + chr(255) ## Blue
+	
+		for j in range(light,FIXTURES):
+			data = data + chr(0) ## Red
+			data = data + chr(0) ## Green
+			data = data + chr(0) ## Blue
+	
+		try:
+			sock.send(data)
+		except socket.error as msg:
+			print msg
+			break
+		time.sleep(0.0225)
+
+sock.close()
